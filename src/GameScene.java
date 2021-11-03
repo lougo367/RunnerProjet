5d2b586f227a63d26bafd372e41a155dffe2370a
lougo367 <louis.goetz@ensea.fr>
2021-11-03 18:27:09 +0100

    Intuitive staticThing
    Modification of staticThing in a order to make more easier to use. The origine coordinates of a staticThing object or no longer expected to be re-defined in the parent code.

--- src/GameScene.java
+++ src/GameScene.java
@@ -5,6 +5,8 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
+import java.io.Serial;
+
 public class GameScene extends Scene {
 
     private Camera camera;
@@ -26,8 +28,8 @@ public class GameScene extends Scene {
 
 
     public void render(){//modification du nom
-        this.root.getChildren().add(this.bckLeft.getView());//ajoute une image à la scène à la racine : voir graphe sujet figure 1
-        this.root.getChildren().add(this.bckRight.getView());
+        this.root.getChildren().addAll(this.bckLeft.getView());//ajoute une image à la scène à la racine : voir graphe sujet figure 1
+        this.root.getChildren().addAll(this.bckRight.getView());
     }
     public GameScene(Group g,int height, int width){// Group groupe d'éléments affichables propres à javafx
         super(g,width,height);
@@ -37,6 +39,9 @@ public class GameScene extends Scene {
         this.wWindow = width;
         this.bckLeft = new staticThing("desert.png",0,0,800,400);
         this.bckRight = new staticThing("desert.png",800,0,800,400);
+        //System.out.println("Control of the value of bckRight GameScene constructor");
+        //System.out.println(bckRight.getView().getX());
+        //System.out.println(bckRight.getX());
         this.root = g;
 
 
