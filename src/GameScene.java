07e53f5be042d680614137b0779313ec97acbf4c
lougo367 <louis.goetz@ensea.fr>
2021-11-03 16:12:38 +0100

    Update GameScene.java

--- src/GameScene.java
+++ src/GameScene.java
@@ -6,12 +6,13 @@ import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
 public class GameScene extends Scene {
-    Camera camera;
+    private Camera camera;
     private static int wWindow;
     private static int hWindow;
     private static int numberOfLives;
     private staticThing bckgrnd;
     private Group root;
+    private Scene scene;
 
     public int getwWindow() {
         return wWindow;
@@ -20,12 +21,13 @@ public class GameScene extends Scene {
     public int gethWindow() {
         return hWindow;
     }
+
     public void render(Stage primaryStage){
         primaryStage.setTitle("Demo");
 
 
         primaryStage.setScene(this);
-        root.getChildren().add(imageViewSceneryLeft);//ajoute une image à la scène à la racine : voir graphe sujet figure 1
+        root.getChildren().add(this.bckgrnd.getView());//ajoute une image à la scène à la racine : voir graphe sujet figure 1
         primaryStage.show();
     }
     public GameScene(Group g,int height, int width){// Group groupe d'éléments affichables propres à javafx
@@ -36,5 +38,6 @@ public class GameScene extends Scene {
         this.wWindow = width;
         this.bckgrnd = new staticThing("src/desert.png",0,0);
         this.root = g;
+        //modification
     }
 }
